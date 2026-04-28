b62eaa0ea24ef42dff04f219896acf1d808f3a28
sameerasw <sameerasw.com@gmail.com>
2026-04-29 02:52:17 +0530

    chore: lower minimum SDK version to 30 in build configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
 
     defaultConfig {
         applicationId = "com.sameerasw.livepaper"
-        minSdk = 33
+        minSdk = 30
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
