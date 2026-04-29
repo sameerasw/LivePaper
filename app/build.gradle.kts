65e531efdc14e0261c9050edeef0144a21dd70fb
sameerasw <sameerasw.com@gmail.com>
2026-04-29 09:53:14 +0530

    chore: bump application version code to 2 and version name to 2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.sameerasw.livepaper"
         minSdk = 30
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
